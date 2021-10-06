42092cd80d721aff8f6cda91f56bcac40172b08f
sarahkgh <sarahk.work@outlook.com>
2021-10-06 11:19:04 +0100

    ReturnTypes

---
+++ ReturnTypes/src/ReturnTypes.java
@@ -0,0 +1,30 @@
+
+public class ReturnTypes {
+	public static int returnInt() {
+		return 36;
+	}
+	
+	public static String stringstr() {
+		return "Some string here";
+	}
+	
+	public static int returnIntegar() {
+		return 25;
+	}
+	
+	public static boolean returnTrue() {
+		return true;
+	}
+	
+	public static char newChar() {
+		return 'a';
+	}
+	public static float returnFlo() {
+		return 0.85f;
+				
+	}
+	public static void returnNothing() {
+		return;
+	}
+
+}
---
+++ ReturnTypes/src/Runner.java
@@ -0,0 +1,28 @@
+
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		 helloWorld();
+		 System.out.println(ReturnTypes.stringstr());
+		 System.out.println(ReturnTypes.returnIntegar());
+		 System.out.println(ReturnTypes.returnTrue());
+		 System.out.println(ReturnTypes.newChar());
+		
+		 System.out.println(ReturnTypes.returnFlo());
+		 
+		 
+	}
+	
+	public static void helloWorld() {
+		System.out.println("Hello World");
+	}
+	
+	public static void boo() {
+		System.out.println("spoooooooky");
+		
+		
+		
+	}
+
+}
